9508d2c6d31d2f946f75a093f78ccb155aa48a50
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-10-15 23:14:38 +0300

    two sum problem solved with two pointers approach

---
+++ _Two_Pointers.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<pair<int, int>> numWithIndex;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            numWithIndex.push_back({nums[i], i});
+        }
+
+        sort(numWithIndex.begin(), numWithIndex.end());
+
+        int left = 0;
+        int right = numWithIndex.size() - 1;
+
+        while (left < right)
+        {
+            int sum = numWithIndex[left].first + numWithIndex[right].first;
+
+            if (sum == target)
+            {
+                return {numWithIndex[left].second, numWithIndex[right].second};
+            }
+
+            if (sum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return {};
+    }
+};
